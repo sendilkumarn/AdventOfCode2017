adf7868814e3e2d5510ac9ff2e30b6f59c6929a2
Sendil Kumar <sendilkumarn@live.com>
2017-12-15 20:41:45 +0100

    js in Day 14

--- day10/aocd10s2.py
+++ day10/aocd10s2.py
@@ -1,25 +1,33 @@
-out = "70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41"
+# out = "70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41"
 
-shiftLen = [ord(x) for x in out] + [17, 31, 73, 47, 23]
+s="hxtvlmkl"
 
-current = skip = 0
-inputList = list(range(256))
-size = len(inputList)
-for _ in range(64):
-    for shift in shiftLen:
-        l = int(shift)
-        for i in range(l // 2):
-            start = (current + i) % size
-            end = (current + l - 1 - i) % size
-            inputList[start], inputList[end] = inputList[end], inputList[start]
-        current += l + skip
-        skip += 1
+def convertToHash(val):
+    shiftLen = [ord(x) for x in val] + [17, 31, 73, 47, 23]
 
-hashstr = ""
-for i in range(len(inputList) // 16):
-    num = 0
-    for j in range(16):
-        num ^= inputList[i * 16 + j]
-    hashstr += hex(num)[2:].zfill(2)
+    current = skip = 0
+    inputList = list(range(256))
+    size = len(inputList)
+    for _ in range(64):
+        for shift in shiftLen:
+            l = int(shift)
+            for i in range(l // 2):
+                start = (current + i) % size
+                end = (current + l - 1 - i) % size
+                inputList[start], inputList[end] = inputList[end], inputList[start]
+            current += l + skip
+            skip += 1
 
-print(hashstr)
+    hashstr = ""
+    for i in range(len(inputList) // 16):
+        num = 0
+        for j in range(16):
+            num ^= inputList[i * 16 + j]
+        hashstr += hex(num)[2:].zfill(2)
+
+    return hashstr
+
+
+for i in xrange(128):
+    hashstr = convertToHash(s+"-"+str(i))
+    print hashstr
